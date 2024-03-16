2153d2b7fcc2bdaff54158a3bd4205cc818b9c05
sooheyon <sooheyonjo@gmail.com>
2024-03-16 09:41:28 +0900

    first commit

---
+++ vite/README.md
@@ -0,0 +1,5 @@
+# To Do List
+
+- vite
+- react
+- supabase
---
+++ vite/src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return <div className="bg-red-100">hello world</div>;
+};
+
+export default App;
